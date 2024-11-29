362279fbb702fb15b9935274562f627954697f2d
srouke <xpyctam4@yandex.ru>
2024-11-29 16:49:50 +0500

    test

--- a-library-for-others/main.go
+++ a-library-for-others/main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strings"
+)
+
+type CSVParser interface {
+	ReadLine(r io.Reader) (string, error)
+	GetField(n int) (string, error)
+	GetNumberOfFields() int
+}
+
+type csvParser struct {
+	line     string
+	fields   []string
+	lineRead bool
+}
+
+func (p csvParser) ReadLine(r io.Reader) (string, error) {
+	reader := bufio.NewReader(r)
+	line, err := reader.ReadString('\n')
+	if err != nil && err != io.EOF {
+		return "", err
+	}
+	p.line = strings.TrimSuffix(line, "\n")
+	p.fields = strings.Split(p.line, ",")
+	p.lineRead = true
+	return p.line, nil
+}
+
+func (mp csvParser) GetField(n int) (string, error) {
+	return "", nil
+}
+
+func (mp csvParser) GetNumberOfFields() int {
+	return 0
+}
 
 func main() {
-	fmt.Println("kek")
+	var parser CSVParser
+	parser = csvParser{}
+
+	reader := strings.NewReader("kek\nasd")
+
+	fmt.Println(parser.ReadLine(reader))
 }
